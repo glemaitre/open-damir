f41b2087184e1bbf60018054838fd7facc95a0f5
Gael Varoquaux <gael.varoquaux@normalesup.org>
2019-04-08 15:07:00 +0200

    sym_link

--- preprocessing.py
+++ preprocessing.py
@@ -3,7 +3,7 @@ import os
 import dask.dataframe as dd
 import pandas as pd
 
-path_data = '/home/lemaitre/Downloads/OpenDamir'
+path_data = 'data'
 
 lexicon_filename = os.path.join(
     path_data, 'Lexique_open-DAMIR.xls'
