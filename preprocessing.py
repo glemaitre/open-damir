7e254a191a9f70bf02279ff8965fdd94ebe877cf
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2019-04-08 15:10:51 +0200

    iter

--- preprocessing.py
+++ preprocessing.py
@@ -1,18 +1,24 @@
+#%%
 import os
 
 import dask.dataframe as dd
 import pandas as pd
 
-path_data = '/home/lemaitre/Downloads/OpenDamir'
-
+#%%
+path_data = 'data'
 lexicon_filename = os.path.join(
     path_data, 'Lexique_open-DAMIR.xls'
 )
-
 sample_filename = os.path.join(
     path_data, 'A201501.csv'
 )
 
+#%%
+# Read a sample file to get the column
+df_sample = dd.read_csv(sample_filename, sep=';', usecols=range(55))
+df_sample.head()
 
-df_month = dd.read_csv(sample_filename, sep=';')
-
+#%%
+df_lexicon = pd.read_excel(
+    lexicon_filename,
+)
