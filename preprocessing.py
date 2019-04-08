08713a581e1b85e9d5949cf5c8aa93977c26f0e2
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2019-04-08 14:56:42 +0200

    Initial commit

---
+++ preprocessing.py
@@ -0,0 +1,18 @@
+import os
+
+import dask.dataframe as dd
+import pandas as pd
+
+path_data = '/home/lemaitre/Downloads/OpenDamir'
+
+lexicon_filename = os.path.join(
+    path_data, 'Lexique_open-DAMIR.xls'
+)
+
+sample_filename = os.path.join(
+    path_data, 'A201501.csv'
+)
+
+
+df_month = dd.read_csv(sample_filename, sep=';')
+
