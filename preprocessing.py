cfb50d7d3674704b7619ff4fada52f0157c9d0f0
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2019-04-08 15:51:14 +0200

    lexicon reading

--- preprocessing.py
+++ preprocessing.py
@@ -19,6 +19,23 @@ df_sample = dd.read_csv(sample_filename, sep=';', usecols=range(55))
 df_sample.head()
 
 #%%
-df_lexicon = pd.read_excel(
-    lexicon_filename,
+COL_TO_IGNORE = (
+    'FLX_ANN_MOI',
+    'PRS_ACT_COG', 'PRS_ACT_NBR', 'PRS_ACT_QTE',
+    'PRS_DEP_MNT', 'PRS_PAI_MNT',
+    'PRS_REM_BSE', 'PRS_REM_MNT', 'PRS_REM_TAU',
+    'FLT_ACT_COG', 'FLT_ACT_NBR', 'FLT_ACT_QTE',
+    'FLT_PAI_MNT',
+    'FLT_DEP_MNT',
+    'FLT_REM_MNT',
+    'SOI_ANN', 'SOI_MOI'
 )
+
+lexicon = {}
+for col_name in df_sample.columns:
+    if col_name in COL_TO_IGNORE:
+        continue
+    lexicon[col_name] = pd.read_excel(
+        lexicon_filename, sheet_name=col_name, index_col=0
+    )
+#%%
