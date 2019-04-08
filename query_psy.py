f7618f7770ab2292421ebec46804c51002426b10
Gael Varoquaux <gael.varoquaux@normalesup.org>
2019-04-08 17:49:07 +0200

    Also neuro

--- query_psy.py
+++ query_psy.py
@@ -22,19 +22,19 @@ code = []
 for key in KEYWORDS:
     code.append(
         df_act_cat['Libellé Nature de Prestation'][
-            df_act_cat['Libellé Nature de Prestation'].str.contains('PSYCH')
+            df_act_cat['Libellé Nature de Prestation'].str.contains(key)
         ]
     )
 code = pd.concat(code, axis=0)
 
 #%%
 sample_filename = os.path.join(
-    path_data, '2015*.csv'
+    path_data, 'A*.csv'
 )
 df_sample = dd.read_csv(sample_filename, sep=';', usecols=range(55))
 df_sample = df_sample[df_sample['PRS_NAT'].isin(code.index)]
 
 #%%
-df_sample.to_parquet('psy.parquet')
+df_sample.to_parquet(os.path.join(path_data, 'psy.parquet'))
 
 #%%
