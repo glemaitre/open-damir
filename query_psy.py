443a42db4f2df5ce3b56eca374f691a349f7d1e5
Gael Varoquaux <gael.varoquaux@normalesup.org>
2019-04-10 09:14:27 +0200

    Make data uniform

--- query_psy.py
+++ query_psy.py
@@ -1,6 +1,7 @@
 #%%
 import os
 import shutil
+import glob
 
 import dask.dataframe as dd
 import pandas as pd
@@ -31,16 +32,31 @@ code = pd.concat(code, axis=0)
 
 #%%
 # Early years
+
+
 sample_filename = os.path.join(
-    path_data, 'P200911.csv'
-    #path_data, 'P*.csv'
+    #path_data, 'P20091*.csv'
+    path_data, 'P*.csv'
 )
+first_file = sorted(glob.glob(sample_filename))[0]
+df_first = dd.read_csv(first_file, sep=';', usecols=range(55))
+dtypes = df_first.dtypes.to_dict()
+#dtypes['PSP_ACT_SNDS'] = 'float32'
+dtypes_floats = dtypes.copy()
+for key, value in dtypes.items():
+    if value.kind == 'i':
+        # Convert to float32 to be robust to nans
+        dtypes_floats[key] = np.float32
+
 df_sample = dd.read_csv(sample_filename, sep=';', usecols=range(55),
                 # We need to coerce dtypes, because sometimes there are
                 # missing values
-                dtype={'PSP_ACT_SNDS': 'float64'})
+                dtype=dtypes_floats)
 df_sample = df_sample[df_sample['PRS_NAT'].isin(code.index)]
-
+df_sample.fillna(-999)
+for key in dtypes.keys():
+    if dtypes[key] != dtypes_floats[key]:
+        df_sample[key] = df_sample[key].astype(dtypes[key])
 
 #%%
 zeat_to_gps = pd.read_csv('zeat_to_gps.csv')
@@ -63,7 +79,10 @@ for col in df_sample.columns:
 
 #%%
 out_filename = os.path.join(path_data, 'clean_P.parquet')
-shutil.rmtree(out_filename)
+try:
+    shutil.rmtree(out_filename)
+except:
+    pass
 df_sample.to_parquet(out_filename)
 
 print('Done with the P files')
@@ -71,7 +90,7 @@ print('Done with the P files')
 ##%%
 ## Later years
 #sample_filename = os.path.join(
-#    path_data, 'A201709.csv'
+##    path_data, 'A201709.csv'
 #    path_data, 'A*.csv'
 #)
 #df_sample = dd.read_csv(sample_filename, sep=';', usecols=range(55),
@@ -102,5 +121,7 @@ print('Done with the P files')
 #
 #
 ##%%
-#df_sample.to_parquet(os.path.join(path_data, 'clean_A.parquet'))
-
+#out_filename = os.path.join(path_data, 'clean_A.parquet')
+#shutil.rmtree(out_filename)
+#df_sample.to_parquet(out_filename)
+#
